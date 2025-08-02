13725ea877d51decbbff061dbee5bc28b5e1456e
Fabien Rajaonarison <rajaonarisonfabien@yahoo.fr>
2025-08-02 19:19:05 +0900

    fix: lint

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 require 'options'
@@ -106,13 +106,13 @@ require 'lazy-bootstrap'
 require 'lazy-plugins'
 
 -- Setup globals
-require('globals')
+require 'globals'
 
 -- Setup auto commands
-require('autocmd')
+require 'autocmd'
 
 -- Go to last tab
-require('last-tab')
+require 'last-tab'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -53,8 +53,8 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
 
@@ -91,4 +91,3 @@ require('lazy').setup({
 })
 
 -- vim: ts=2 sts=2 sw=2 et
-
